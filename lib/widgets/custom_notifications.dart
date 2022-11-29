997df27db0c8d29058c2f03310162ee72670aafa
Shalom Abrokwah <shalomabrokwah@gmail.com>
2022-11-29 15:34:56 +0100

    Created the notification widget

---
+++ lib/widgets/custom_notifications.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class NotificationIcon extends StatelessWidget {
+  const NotificationIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topRight,
+      children: [
+        Icon(
+          CupertinoIcons.bell,
+          color: const Color(0xff111827),
+          size: 21.w,
+        ),
+        Container(
+          height: 9.h,
+          width: 9.w,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100).r,
+              color: const Color(0xff004852)),
+        )
+      ],
+    );
+  }
+}
